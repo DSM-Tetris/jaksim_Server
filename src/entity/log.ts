336e4ff9042a9a3912954e1d1f0f26f671bad455
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-12 15:41:28 +0900

    feat: log type enum

--- src/entity/log.ts
+++ src/entity/log.ts
@@ -1,16 +1,27 @@
 import { Field, ID, ObjectType } from "type-graphql";
 import { User } from "./user";
+import { LogType } from "./logType";
 
 @ObjectType()
 export class Log {
+  constructor(date: Date, type: LogType, user: User) {
+    this.date = date;
+    this.type = type;
+    this.user = user;
+  }
+
+  static createLoginLog(user: User) {
+    return new Log(new Date(), LogType.LOGIN, user);
+  }
+
   @Field((type) => ID)
   readonly id!: number;
 
   @Field()
   date!: Date;
 
-  @Field()
-  type!: string;
+  @Field((type) => LogType)
+  type!: LogType;
 
   @Field((type) => User)
   user!: User;

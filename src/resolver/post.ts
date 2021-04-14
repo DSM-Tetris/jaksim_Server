74a0aa53581695d60238295ef41718b943742443
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-14 10:17:29 +0900

    feat: post resolver

--- src/resolver/post.ts
+++ src/resolver/post.ts
@@ -1,14 +1,25 @@
-import { Resolver, Query, UseMiddleware, Arg } from "type-graphql";
-import { GetPostsRequest, GetPostsResult } from "../dto";
+import { Resolver, Query, UseMiddleware, Arg, Mutation } from "type-graphql";
+import { GetPostsRequest, GetPostsResult, UploadPostResult } from "../dto";
 import { Post } from "../entity";
 import { auth } from "../middleware";
 import { PostService } from "../service/post";
+import { UploadPostRequest } from "../dto/request/uploadPost";
+import { GraphQLUpload } from "graphql-upload";
+import { Upload } from "../type/upload";
 
 @Resolver(Post)
 export class PostResolver {
   @Query(() => GetPostsResult)
   @UseMiddleware(auth)
-  async getPosts(@Arg("data") data: GetPostsRequest): Promise<typeof GetPostsResult> {
+  async getPosts(
+    @Arg("data") data: GetPostsRequest
+  ): Promise<typeof GetPostsResult> {
     return await PostService.getPosts(data);
   }
+
+  @Mutation(() => UploadPostResult)
+  async uploadPost(
+    @Arg("data") data: UploadPostRequest,
+    @Arg("picture", () => GraphQLUpload) { createReadStream, filename }: Upload
+  ) {}
 }

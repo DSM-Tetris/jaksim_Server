9a91ce8311739db0427911d05f0568c3041f82bd
MinJunSeo <smj0385@naver.com>
2021-04-14 14:39:27 +0900

    fix: joi schema condition

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -4,8 +4,8 @@ export const signupSchema = Joi.object()
   .keys({
     username: Joi.string().min(6).max(8).alphanum().required(),
     password: Joi.string().min(8).max(20)
-      .regex(/^(?=.*[A-Za-z])(?=.*[0-9])[~`!@#$%^&*()_+\-=\[\]\\|;':",./<>?a-zA-Z0-9]$/).required(),
+      .regex(/(?=.*\d)(?=.*[A-Za-z])[~`!@#$%^&*()_+\-=\[\]\\|;':",./<>?a-zA-Z0-9]{8,}/).required(),
     email: Joi.string().email().max(25).required(),
-    nickname: Joi.string().min(2).max(6).regex(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]+$/).required(),
-    authCode: Joi.string().length(6).regex(/^[0-9]$/).required()
+    nickname: Joi.string().min(2).max(6).regex(/[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]{2, 6}/).required(),
+    authCode: Joi.string().length(6).regex(/^[0-9]{6}$/).required()
   });

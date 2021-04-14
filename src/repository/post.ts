cea58834b3674f26d9e2bfc30e8373112be136aa
MinJunSeo <smj0385@naver.com>
2021-04-14 16:56:09 +0900

    feat: retrun value and arguments type def

--- src/repository/post.ts
+++ src/repository/post.ts
@@ -2,7 +2,10 @@ import { Post } from "../entity";
 import { context } from "../context";
 
 export class PostRepository {
-  static async findManyByUsername(username: string, page: number) {
+  static async findManyByUsername(
+    username: string,
+    page: number
+  ): Promise<Post[] | null> {
     return await context.prisma.post.findMany({
       where: { username },
       skip: page * 2,

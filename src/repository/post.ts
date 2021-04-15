b57038e1cc69b8dcfa032a5b82f0b82cdb965471
MinJunSeo <smj0385@naver.com>
2021-04-15 18:58:24 +0900

    fix: skip operation

--- src/repository/post.ts
+++ src/repository/post.ts
@@ -8,8 +8,8 @@ export class PostRepository {
   ): Promise<Post[]> {
     return await context.prisma.post.findMany({
       where: { username },
-      skip: page * 2,
-      take: 2
+      skip: (page - 1) * 2,
+      take: 2,
     });
   }
 }

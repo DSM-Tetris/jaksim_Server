e0f3c3a5aa76b3de86e063779e7124538b168b02
MinJunSeo <smj0385@naver.com>
2021-04-15 19:32:00 +0900

    feat: GetPosts dto ffield

--- src/dto/response/getPosts.ts
+++ src/dto/response/getPosts.ts
@@ -1,4 +1,5 @@
 import { ObjectType, Field, createUnionType } from "type-graphql";
+import { Length, MaxLength } from "class-validator";
 
 enum GetPostsMessage {
   SuccessGetPosts = "SUCCESS GET POSTS",
@@ -6,9 +7,33 @@ enum GetPostsMessage {
 
 @ObjectType()
 export class GetPosts {
-  constructor() {
+  constructor(
+    title: string,
+    contentPreview: string,
+    image: string,
+    tags: string[] | null
+  ) {
+    this.title = title;
+    this.contentPreview = contentPreview;
+    this.image = image;
+    this.tags = tags;
     this.message = GetPostsMessage.SuccessGetPosts;
   }
+
+  @Field()
+  @Length(1, 45)
+  title: string;
+
+  @Field()
+  @MaxLength(100)
+  contentPreview: string;
+
+  @Field()
+  @Length(1, 45)
+  image: string;
+
+  @Field()
+  tags: string[] | null;
 
   @Field()
   message: string;

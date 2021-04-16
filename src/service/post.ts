f73306814a20111889800df9e4a3cb6dc05077c2
MinJunSeo <smj0385@naver.com>
2021-04-16 17:00:33 +0900

    feat: getPosts method

--- src/service/post.ts
+++ src/service/post.ts
@@ -1,8 +1,14 @@
-import { GetPostsRequest, Unauthorized } from "../dto";
+import {
+  GetPostsRequest,
+  PostPreview,
+  GetPosts,
+  Unauthorized,
+  NotFoundPost,
+} from "../dto";
 import { usernameSchema } from "../schema";
 import { validateArguments } from "../util";
 import { context } from "../context";
-import { PostRepository } from "../repository";
+import { PostRepository, TagRepository } from "../repository";
 
 export class PostService {
   static async getPosts({ username, page }: GetPostsRequest) {
@@ -16,6 +22,18 @@ export class PostService {
     }
 
     const posts = await PostRepository.findManyByUsername(username, page);
-    console.log(posts);
+    const response: PostPreview[] = [];
+
+    for (const post of posts) {
+      const tags = await TagRepository.findByPostId(post.id);
+      response.push(new PostPreview(
+        post.title,
+        post.content ? post.content.slice(0, 100) : null,
+        post.image,
+        tags
+      ));
+    }
+
+    return posts.length ? new GetPosts(response) : new NotFoundPost();
   }
 }

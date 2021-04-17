7fb96cbe0b2407c1b460c798a119befaca2ff678
MinJunSeo <smj0385@naver.com>
2021-04-17 10:05:39 +0900

    feat: get post resolver

--- src/resolver/post.ts
+++ src/resolver/post.ts
@@ -1,5 +1,5 @@
 import { Resolver, Query, UseMiddleware, Arg } from "type-graphql";
-import { GetPostsRequest, GetPostsResult } from "../dto";
+import { GetPostsRequest, GetPostsResult, GetPostRequest, GetPostResult } from "../dto";
 import { Post } from "../entity";
 import { auth } from "../middleware";
 import { PostService } from "../service/post";
@@ -10,5 +10,11 @@ export class PostResolver {
   @UseMiddleware(auth)
   async getPosts(@Arg("data") data: GetPostsRequest): Promise<typeof GetPostsResult> {
     return await PostService.getPosts(data);
+  }
+
+  @Query(() => GetPostResult)
+  @UseMiddleware(auth)
+  async getPost(@Arg("data") data: GetPostRequest): Promise<typeof GetPostResult> {
+    return await PostService.getPost(data);
   }
 }

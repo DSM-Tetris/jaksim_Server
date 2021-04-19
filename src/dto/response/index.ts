84eca3294268b311f4d2e270ac5ed00831cda29c
MinJunSeo <smj0385@naver.com>
2021-04-19 15:11:06 +0900

    refactor: remove getPosts dto

--- src/dto/response/index.ts
+++ src/dto/response/index.ts
@@ -2,7 +2,6 @@ export * from "./signup";
 export * from "./verifyEmail";
 export * from "./sendEmail";
 export * from "./login";
-export * from "./getPosts";
 export * from "./getPost";
 
 export * from "./badRequest";

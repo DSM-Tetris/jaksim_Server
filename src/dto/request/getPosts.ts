7159f144ea450426f30e8415b9481f1c61621976
MinJunSeo <smj0385@naver.com>
2021-04-19 14:55:35 +0900

    merge: from feature/get-all-posts

--- src/dto/request/getPosts.ts
+++ src/dto/request/getPosts.ts
@@ -1,12 +1,8 @@
 import { InputType, Field } from "type-graphql";
-import { Length, Min } from "class-validator";
+import { Min } from "class-validator";
 
 @InputType()
 export class GetPostsRequest {
-  @Field()
-  @Length(6, 8)
-  username!: string;
-
   @Field()
   @Min(1)
   page!: number;

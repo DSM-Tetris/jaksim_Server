4514c14e0758f700f29935d5078288a765c209a0
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-19 20:04:36 +0900

    feat: post save method

--- src/repository/post.ts
+++ src/repository/post.ts
@@ -12,7 +12,26 @@ export class PostRepository {
       take: 10,
       orderBy: {
         createdAt: "desc",
-      }
+      },
+    });
+  }
+
+  static async save({
+    username,
+    title,
+    content,
+    categoryId,
+    image,
+  }: Post): Promise<void> {
+    await context.prisma.post.create({
+      data: {
+        username: username,
+        title,
+        content,
+        categoryId: categoryId!,
+        image,
+        createdAt: new Date(),
+      },
     });
   }
 }

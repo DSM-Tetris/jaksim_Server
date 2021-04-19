77896ba74ad56166a6e20b660a006239b2ab3919
MinJunSeo <smj0385@naver.com>
2021-04-19 15:28:59 +0900

    refactor: remove findMany method

--- src/repository/post.ts
+++ src/repository/post.ts
@@ -2,17 +2,6 @@ import { Post } from "../entity";
 import { context } from "../context";
 
 export class PostRepository {
-  static async findManyByUsername(
-    username: string,
-    page: number
-  ): Promise<Post[]> {
-    return await context.prisma.post.findMany({
-      where: { username },
-      skip: (page - 1) * 10,
-      take: 10,
-    });
-  }
-
   static async findOneByPostId(postId: number): Promise<Post | null> {
     return await context.prisma.post.findUnique({
       where: { id: postId }

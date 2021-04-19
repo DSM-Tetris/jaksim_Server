7bf25ec468067743a9723ad251c9464a0e0e1625
MinJunSeo <smj0385@naver.com>
2021-04-19 14:55:20 +0900

    refactor: not use username input args

--- src/dto/request/getPost.ts
+++ src/dto/request/getPost.ts
@@ -1,12 +1,8 @@
 import { InputType, Field } from "type-graphql";
-import { Length, Min } from "class-validator";
+import { Min } from "class-validator";
 
 @InputType()
 export class GetPostRequest {
-  @Field()
-  @Length(6, 8)
-  username!: string;
-
   @Field()
   @Min(1)
   postId!: number;

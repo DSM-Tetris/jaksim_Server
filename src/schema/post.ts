f78cb56e6bf43c9a1c57ff8a454459a622194e85
MinJunSeo <smj0385@naver.com>
2021-04-21 09:05:34 +0900

    feat: categoryId prop auth

--- src/schema/post.ts
+++ src/schema/post.ts
@@ -10,4 +10,5 @@ export const getPostsSchema = Joi.object()
   .keys({
     username: Joi.string().min(6).max(8).alphanum().required(),
     page: Joi.number().integer().min(1).required(),
+    categoryId: Joi.number().integer().min(1),
   });

48caeff7906058943cdbdc89caef268619f8c64f
MinJunSeo <smj0385@naver.com>
2021-04-21 09:00:06 +0900

    merge: feature/get-all-post branch

--- src/resolver/post.ts
+++ src/resolver/post.ts
@@ -1,11 +1,17 @@
 import { Resolver, Query, UseMiddleware, Arg } from "type-graphql";
-import { GetPostRequest, GetPostResult } from "../dto";
+import { GetPostsRequest, GetPostsResult, GetPostRequest, GetPostResult } from "../dto";
 import { Post } from "../entity";
 import { auth } from "../middleware";
 import { PostService } from "../service/post";
 
 @Resolver(Post)
 export class PostResolver {
+  @Query(() => GetPostsResult)
+  @UseMiddleware(auth)
+  async getPosts(@Arg("data") data: GetPostsRequest): Promise<typeof GetPostsResult> {
+    return await PostService.getPosts(data);
+  }
+
   @Query(() => GetPostResult)
   @UseMiddleware(auth)
   async getPost(@Arg("data") data: GetPostRequest): Promise<typeof GetPostResult> {

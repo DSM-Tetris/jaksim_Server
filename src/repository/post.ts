1df3b0f11fd2bef219fe4d2b1ea77d29e4388f13
MinJunSeo <smj0385@naver.com>
2021-04-22 09:46:18 +0900

    refactor: page conditions remove

--- src/repository/post.ts
+++ src/repository/post.ts
@@ -4,12 +4,9 @@ import { context } from "../context";
 export class PostRepository {
   static async findManyByUsername(
     username: string,
-    page: number
   ): Promise<Post[]> {
     return await context.prisma.post.findMany({
       where: { username },
-      skip: (page - 1) * 10,
-      take: 10,
       orderBy: {
         createdAt: "desc",
       }

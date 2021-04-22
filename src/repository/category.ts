11fd16dbe82a6bdb97b966814b3c6b4db4498d55
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-22 11:29:21 +0900

    fix: remove useless async

--- src/repository/category.ts
+++ src/repository/category.ts
@@ -2,9 +2,9 @@ import { Category } from "../entity";
 import { context } from "../context";
 
 export class CategoryRepository {
-  static async getCategoryList(username: string): Promise<Category[]> {
+  static getCategoryList(username: string): Promise<Category[]> {
     return context.prisma.category.findMany({
-      where: { username }
+      where: { username },
     });
   }
 }

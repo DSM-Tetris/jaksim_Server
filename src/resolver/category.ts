7a2aba2a76110d11b93939cb620307ee50dee1dc
MinJunSeo <smj0385@naver.com>
2021-04-26 15:01:34 +0900

    refactor: remove not use module

--- src/resolver/category.ts
+++ src/resolver/category.ts
@@ -1,13 +1,11 @@
 import { Resolver, Query, UseMiddleware } from "type-graphql";
 import { Category } from "../entity";
-import { Validate } from "../decorator/validateArguments";
 import { auth } from "../middleware";
 import { CategoryService } from "../service";
 import { GetCategoryListResult } from "../dto";
 
 @Resolver(Category)
 export class CategoryResolver {
-  @Validate
   @Query(() => GetCategoryListResult)
   @UseMiddleware(auth)
   async getCategoryList(): Promise<typeof GetCategoryListResult> {

ebc4b276dbffb82b7ca8795c86b94d853e1345c8
MinJunSeo <smj0385@naver.com>
2021-04-26 13:37:16 +0900

    refactor: getCategoryList method

--- src/service/category.ts
+++ src/service/category.ts
@@ -3,23 +3,30 @@ import { PostRepository } from "../repository/post";
 
 export class CategoryService {
   static async getCategoryList(username: string) {
-    const categoryList: { [key: string]: number } = {};
+    const categoryList = {};
+    const posts = await PostRepository.groupPostByCategoryId(username);
     const categories = await CategoryRepository.findManyByUsername(username);
+    let numOfAllPost = Number(posts[0].count);
+
+    for (let i = 1; i < posts.length; i++) {
+      const post = posts[i];
+      numOfAllPost += Number(post.count);
+
+      for (let j = 0; j < categories.length; j++) {
+        if (post.categoryId === categories[j].id) {
+          categoryList[categories[j].name] = post.count;
+          categories.splice(j, 1);
+          j--;
+          break;
+        }
+      }
+    }
 
     for (const category of categories) {
       categoryList[category.name] = 0;
     }
-
-    const posts = await PostRepository.findManyByUsername(username);
     
-    categoryList["전체 카테고리"] = posts.length;
-    for (const post of posts) {
-      if (post.category) {
-        const categoryName = post.category.name;
-        categoryList[categoryName] += 1;
-      }
-    }
-
+    categoryList["전체 카테고리"] = numOfAllPost;
     return categoryList;
   }
 }

6ad4cc3ca4b851c6ec3f2a795653cae69190ba80
MinJunSeo <smj0385@naver.com>
2021-04-27 20:02:30 +0900

    refactor: add username property

--- src/dto/request/modifyPassword.ts
+++ src/dto/request/modifyPassword.ts
@@ -4,6 +4,10 @@ import { Length } from "class-validator";
 @InputType()
 export class ModifyPasswordRequest {
   @Field()
+  @Length(6, 8)
+  username!: string;
+
+  @Field()
   @Length(8, 20)
   oldPassword!: string;
 

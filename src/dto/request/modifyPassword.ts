7a18ff8046f6ec493f5091c481efde3a3165dacb
MinJunSeo <smj0385@naver.com>
2021-04-27 20:07:52 +0900

    refactor: modify password by username -> password

--- src/dto/request/modifyPassword.ts
+++ src/dto/request/modifyPassword.ts
@@ -1,15 +1,12 @@
 import { InputType, Field } from "type-graphql";
-import { Length } from "class-validator";
+import { IsEmail, MaxLength, Length } from "class-validator";
 
 @InputType()
 export class ModifyPasswordRequest {
   @Field()
-  @Length(6, 8)
-  username!: string;
-
-  @Field()
-  @Length(8, 20)
-  oldPassword!: string;
+  @IsEmail()
+  @MaxLength(25)
+  email!: string;
 
   @Field()
   @Length(8, 20)

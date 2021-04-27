d0e34909cea1bb9c360bfee695e0c9a31c493fce
MinJunSeo <smj0385@naver.com>
2021-04-28 08:57:03 +0900

    refactor: export modify password response dto

--- src/dto/response/index.ts
+++ src/dto/response/index.ts
@@ -7,6 +7,7 @@ export * from "./uploadPost";
 export * from "./getPosts";
 export * from "./getPost";
 export * from "./getCategory";
+export * from "./modifyPassword";
 
 export * from "./badRequest";
 export * from "./unauthorized";

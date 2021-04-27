1505ad46dee4d6686c8bfef20def55001267cc52
MinJunSeo <smj0385@naver.com>
2021-04-27 19:46:19 +0900

    refactor: rename class and file

--- src/dto/request/modifyPassword.ts
+++ src/dto/request/modifyPassword.ts
@@ -2,7 +2,7 @@ import { InputType, Field } from "type-graphql";
 import { Length } from "class-validator";
 
 @InputType()
-export class ChangePasswordRequest {
+export class ModifyPasswordRequest {
   @Field()
   @Length(8, 20)
   oldPassword!: string;

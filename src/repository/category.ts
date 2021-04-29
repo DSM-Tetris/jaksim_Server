54c1beb8836ee8bbb1ea5ac4bc12a9a2b4b09e01
MinJunSeo <smj0385@naver.com>
2021-04-29 09:54:47 +0900

    style: remove async keyword

--- src/repository/category.ts
+++ src/repository/category.ts
@@ -2,7 +2,7 @@ import { Category } from "../entity";
 import { context } from "../context";
 
 export class CategoryRepository {
-  static async findManyByUsername(username: string): Promise<Category[]> {
+  static findManyByUsername(username: string): Promise<Category[]> {
     return context.prisma.category.findMany({
       where: { username },
     });

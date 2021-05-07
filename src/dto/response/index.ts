87ca549686c1dc3609ea0016242645d38c0315d3
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-07 15:01:04 +0900

    feat: remove category response

--- src/dto/response/index.ts
+++ src/dto/response/index.ts
@@ -10,6 +10,7 @@ export * from "./getCategory";
 export * from "./modifyPassword";
 export * from "./addCategory";
 export * from "./modifyCategory";
+export * from "./removeCategory";
 
 export * from "./badRequest";
 export * from "./unauthorized";

c438c53e85eac92750f9a7ff49ec4061da678e87
MinJunSeo <smj0385@naver.com>
2021-05-11 16:56:07 +0900

    feat: findManyByUsername repository method

--- src/repository/log.ts
+++ src/repository/log.ts
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { Log } from "../entity";
 import { context } from "../context";
 
@@ -9,6 +10,21 @@ export class LogRepository {
         type,
         username,
       },
+    });
+  }
+
+  static findManyByUsername(username: string, maxDay: number) {
+    const thisMonth = moment().format("YYYY-MM");
+    const nextMonth = moment().add(1, "m").format("YYYY-MM");
+
+    return context.prisma.log.findMany({
+      where: {
+        date: {
+          gte: new Date(thisMonth).toISOString(),
+          lt: new Date(nextMonth).toISOString(),
+        },
+        username
+      }
     });
   }
 }

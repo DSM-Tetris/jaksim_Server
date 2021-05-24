8f985e5ac0ac9fe5d3291e4846d0de157054b577
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 14:46:54 +0900

    chore:

--- spec/service/user.spec.ts
+++ spec/service/user.spec.ts
@@ -9,6 +9,10 @@ import { JwtGenerator } from "../../src/util";
 import { LoginResponse } from "../../src/dto";
 
 describe("user service test", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("login success", async () => {
     const dto = { username: "dkssud9556", password: "@Passw0rd" };
 
@@ -24,7 +28,40 @@ describe("user service test", () => {
     LogRepository.save = jest.fn().mockResolvedValue(null);
     PasswordService.match = jest.fn().mockResolvedValue(true);
 
-    const result = await UserService.login(dto);
-    expect(result).toBeInstanceOf(LoginResponse.Login);
+    UserService.login(dto)
+      .then((result) => expect(result).toBeInstanceOf(LoginResponse.Login))
+      .catch((err) => fail(err));
+  });
+
+  test("login fail - wrong username", async () => {
+    const dto = { username: "wronguser123", password: "@Passw0rd" };
+
+    UserRepository.findByUsername = jest.fn().mockResolvedValue(null);
+
+    UserService.login(dto)
+      .then((result) =>
+        expect(result).toBeInstanceOf(LoginResponse.InvalidLoginInfo)
+      )
+      .catch((err) => fail(err));
+  });
+
+  test("login fail - wrong password", async () => {
+    const dto = { username: "dkssud9556", password: "@Password1" };
+    const spy = jest.spyOn(UserRepository, "findByUsername");
+
+    UserRepository.findByUsername = jest.fn().mockResolvedValue({
+      username: "dkssud9556",
+      password: "@Passw0rd",
+      nickname: "testuser",
+      email: "test@test.com",
+    });
+    PasswordService.match = jest.fn().mockResolvedValue(false);
+
+    UserService.login(dto)
+      .then((result) => {
+        expect(result).toBeInstanceOf(LoginResponse.InvalidLoginInfo);
+        expect(spy).toBeCalledTimes(1);
+      })
+      .catch((err) => fail(err));
   });
 });
